9889b32d6e43cd7e0bfabd9e270538b04ab70880
Nelson Chen <nelson.chen1999@gmail.com>
2019-06-20 10:00:33 +1000

    Fib

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
 from .models import *
+import os,sys
 
 # Create your views here.
 
@@ -26,6 +27,43 @@ def example_post(request):
 		try:
 			data = request.POST["data"]
 			return JsonResponse({"log":log})
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return HttpResponse("ONLY POST REQUESTS")
+
+@csrf_exempt
+def fib(request):
+	log = []
+	jsob = {"startNumber": 0, "length": 10}
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			recieved = json.loads(data)
+			jsob.update(recieved)
+
+
+			startNumber = int(jsob['startNumber'])
+			length = int(jsob['length'])
+			loop = range(length)
+
+			numarray = []
+
+			fibno = startNumber
+			addno = 1
+
+			for i in loop:
+				numarray.append(fibno)
+				fibno = fibno + addno
+				addno = fibno - addno
+
+
+
+			return JsonResponse({'fib': numarray})
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			other = sys.exc_info()[0].__name__
